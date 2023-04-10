61b3eb4dd5c98d274e32594957abe1a80b3ef6b1
19495 <19495@ALRAWI.myfiosgateway.com>
2023-04-10 12:05:39 -0400

    add method

--- src/Demo2/Test2.java
+++ src/Demo2/Test2.java
@@ -4,5 +4,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Test2 In process...");
 	}
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
 
 }
